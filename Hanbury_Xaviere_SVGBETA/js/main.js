58bd5916f0ad9494e2c84585c2e36c99172c4e16
xavierehanbury1 <113371075+xavierehanbury1@users.noreply.github.com>
2024-02-03 18:27:09 -0500

    Add CSS animation to SVG
    Added all relevant pieces of CSS animation.

--- Hanbury_Xaviere_SVGBETA/js/main.js
+++ Hanbury_Xaviere_SVGBETA/js/main.js
@@ -1,7 +1,7 @@
 //The Window.console property returns a reference to the console object, which provides methods for logging information to the browser's console.
 console.log("JS file connected");
 
-const vectorGraphic = document.querySelector('#starbadge', 'crystalball');
+const vectorGraphic = document.querySelector('#starbadge');
 
 function logThisId() {
     console.log(this);
@@ -18,3 +18,42 @@ function logThisId2() {
 }
 
 vector2Graphic.addEventListener('click', logThisId2);
+
+const vector3Graphic = document.querySelector('#bird');
+
+function logThisId3() {
+    console.log(this);
+    console.log('You met a:', this.id);
+}
+
+vector3Graphic.addEventListener('click', logThisId3);
+
+const vector4Graphic = document.querySelector('#moon');
+
+function logThisId4() {
+    console.log(this);
+    console.log('Behold, the:', this.id);
+}
+
+vector4Graphic.addEventListener('click', logThisId4);
+
+const vector5Graphic = document.querySelector('#sickle');
+
+function logThisId5() {
+    console.log(this);
+    console.log('A cutting edge:', this.id);
+}
+
+vector5Graphic.addEventListener('click', logThisId5);
+
+
+const vector6Graphic = document.querySelector('#symbol');
+
+function logThisId6() {
+    console.log(this);
+    console.log('Look, a:', this.id);
+}
+
+vector6Graphic.addEventListener('click', logThisId6);
+
+
